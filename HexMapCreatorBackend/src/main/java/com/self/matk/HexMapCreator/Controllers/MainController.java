b0bfc96c0120e50730e4c576111a0d18561d8e69
mattm <kumkowskimateusz2002@gmail.com>
2024-10-26 23:43:46 -0400

    Added Dependency Injection into the controller and a test endpoint for the base url. Additionally installed a vite react for the frontend.

--- HexMapCreatorBackend/src/main/java/com/self/matk/HexMapCreator/Controllers/MainController.java
+++ HexMapCreatorBackend/src/main/java/com/self/matk/HexMapCreator/Controllers/MainController.java
@@ -1,7 +1,42 @@
 package com.self.matk.HexMapCreator.Controllers;
 
+import com.self.matk.HexMapCreator.Services.*;
+import org.hibernate.mapping.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    AccountService accountService;
+    HexNodesService hexNodesService;
+    MapsService mapsService;
+    TagListService tagListService;
+    TagsService tagsService;
+
+    @Autowired
+    public MainController(  AccountService accountService,
+                            HexNodesService hexNodesService,
+                            MapsService mapsService,
+                            TagListService tagListService,
+                            TagsService tagsService
+                         )
+    {
+        this.accountService = accountService;
+        this.hexNodesService = hexNodesService;
+        this.mapsService = mapsService;
+        this.tagListService = tagListService;
+        this.tagsService = tagsService;
+    }
+
+    @GetMapping(value = "/")
+    public ResponseEntity mainPage(){
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World!");
+    }
+
 }
