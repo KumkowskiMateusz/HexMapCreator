c848fa383aeca0bf0c69e7db3df50e40b6e7a5b2
mattm <kumkowskimateusz2002@gmail.com>
2024-11-18 19:48:37 -0500

    Replanned a simpler UI, just making the center being a hexagon doing a constant rotation in the center of the page

--- HexMapCreatorFrontend/HexMapCreator/src/pages/Homepage.tsx
+++ HexMapCreatorFrontend/HexMapCreator/src/pages/Homepage.tsx
@@ -6,37 +6,25 @@ function Homepage(){
 
     
         return(
-        <>
-            <div id="hexagon_border_glow" className="absolute">
-                <div id="hexagon_border">
-                        <div id="hexagon"></div> 
-                </div>
+        <>   
+            <div id="hexagon_container">
+                <div id="mid_hexagon">
+                        <div id="hexagon_border">
+                                <div id="hexagon" className="bg_lightshade"></div> 
+                        </div>
+                </div> 
             </div>
-           
-            <div id="selection_holder">
-                <div id="login_form_holder">
-                    <button id="switch_form_button">Switch Form</button>
-                    <form id="login_form">
-                        <label htmlFor="login_username">Username</label>
-                        <input type="text" id="login_username" name="login_username"></input>
-                        <br></br>
-                        <label htmlFor="login_password">Password</label>
-                        <input type="password" id="login_password"></input>
-                        <br></br>
-                        <input type="submit" id="login_submit" value="Login"></input>
-                    </form>
-                </div>
-                <div id="signup_form_holder">
-                    <form id="signup_form">
-                        <label htmlFor="signup_username">Username</label>
-                        <input type="text" id="signup_username" name="signup_username"></input>
-                        <br></br>
-                        <label htmlFor="signup_password">Password</label>
-                        <input type="password" id="signup_password"></input>
-                        <br></br>
-                        <input type="submit" id="signup_submit" value="Sign up!"></input>
-                    </form>
-                </div>
+
+            <div id="selection_holder"> 
+                <form id="login_form">
+                    <label htmlFor="login_username">Username&#9;</label>
+                    <input type="text" id="login_username" name="login_username"></input>
+                    <br></br>
+                    <label htmlFor="login_password">Password&#9;</label>
+                    <input type="password" id="login_password"></input>
+                    <br></br>
+                    <input type="submit" id="login_submit" value="Login"></input>
+                </form>
             </div>
         </>
         )
